19ac21228f02e04d2cef9b5296b50dd6e3f7cb17
Dig Rat <CyberianDigRat@gmail.com>
2024-09-17 01:08:26 -0500

    added comments for .profile and added -y for apt

--- Minecraft/openjdk.sh
+++ Minecraft/openjdk.sh
@@ -5,47 +5,55 @@
 # install the system's default latest openjdk.
 install_default()
 {
-sudo apt update
-sudo apt install default-openjdk
+    sudo apt update
+    sudo apt install -y default-openjdk
 }
 
 # download openjdk 21
 install_openjdk21()
 {
-# Download the OpenJDK 21 binaries
-echo "Downloading OpenJDK 21..."
-wget https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/openjdk-21_linux-x64_bin.tar.gz
+    # Download the OpenJDK 21 binaries
+    echo "Downloading OpenJDK 21..."
+    wget https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/openjdk-21_linux-x64_bin.tar.gz
 
-# Extract the tarball
-echo "Extracting OpenJDK 21..."
-tar -xzf openjdk-21_linux-x64_bin.tar.gz
+    # Extract the tarball
+    echo "Extracting OpenJDK 21..."
+    tar -xzf openjdk-21_linux-x64_bin.tar.gz
 
-# Move the extracted folder to /opt
-echo "Moving JDK to /opt/..."
-sudo mv jdk-21 /opt/
+    # Move the extracted folder to /opt
+    echo "Moving JDK to /opt/..."
+    sudo mv jdk-21 /opt/
 
-# Set environment variables to use OpenJDK 21: Add the following lines to your .bashrc or .profile:
-# export JAVA_HOME=/opt/jdk-21
-# export PATH=$JAVA_HOME/bin:$PATH
-echo "Setting up environment variables..."
-echo 'export JAVA_HOME=/opt/jdk-21' >> ~/.bashrc
-echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+    # Set environment variables to use OpenJDK 21: Add the following lines to your .bashrc or .profile:
+    # export JAVA_HOME=/opt/jdk-21
+    # export PATH=$JAVA_HOME/bin:$PATH
+    echo "Setting up environment variables..."
+    echo 'export JAVA_HOME=/opt/jdk-21' >> ~/.bashrc
+    echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+    #echo 'export JAVA_HOME=/opt/jdk-21' >> ~/.profile
+    #echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.profile
 
-# Apply the changes by sourcing .bashrc
-echo "Applying changes..."
-source ~/.bashrc
+    # Apply the changes by sourcing .bashrc
+    echo "Applying changes..."
+    source ~/.bashrc
+    #source ~/.profile
 
-echo "OpenJDK 21 installation is complete!"
+    echo "OpenJDK 21 installation is complete!"
 }
 
 # Change default JDK if needed.
 change_default()
 {
-sudo update-java-alternatives --set openjdk-21
+    sudo update-java-alternatives --set openjdk-21
 }
 
 # check java version.
 java_version()
 {
-java -version
+    java -version
 }
+
+# Main
+install_default
+install_openjdk21
+java_version
