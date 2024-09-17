cde8a82f36b61bb45af00b65838796be86e98a58
Dig Rat <CyberianDigRat@gmail.com>
2024-09-16 22:16:12 -0500

    fixed method invoke and made minor changes.

--- Minecraft/download_server.sh
+++ Minecraft/download_server.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Function to download the Minecraft server JAR file
-download_minecraft_server() {
+download_minecraft_server()
+{
     # URL for the Minecraft server JAR file
-    SERVER_URL="https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar"
-    SERVER_FILE="minecraft_server.jar"
-
-    echo "Downloading Minecraft server JAR file..."
-
+    URL="https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar"
+    
     # Download the server JAR file using wget
-    wget -O "$SERVER_FILE" "$SERVER_URL"
+    echo "Downloading Minecraft server JAR file..."
+    wget "$SERVER_URL"
 
     # Check if the download was successful
     if [ $? -eq 0 ]; then
@@ -20,4 +19,5 @@ download_minecraft_server() {
     fi
 }
 
-download_minecraft_server()
+# Main
+download_minecraft_server
