d78985bae2865e513e35dab29b5613972e1f4a96
Dig Rat <CyberianDigRat@gmail.com>
2024-09-16 22:06:46 -0500

    script for downloading minecraft server file

---
+++ Minecraft/download_server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Function to download the Minecraft server JAR file
+download_minecraft_server() {
+    # URL for the Minecraft server JAR file
+    SERVER_URL="https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar"
+    SERVER_FILE="minecraft_server.jar"
+
+    echo "Downloading Minecraft server JAR file..."
+
+    # Download the server JAR file using wget
+    wget -O "$SERVER_FILE" "$SERVER_URL"
+
+    # Check if the download was successful
+    if [ $? -eq 0 ]; then
+        echo "Download complete: $SERVER_FILE"
+    else
+        echo "Download failed."
+        exit 1
+    fi
+}
+
+download_minecraft_server()
