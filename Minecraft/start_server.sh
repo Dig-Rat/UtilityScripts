daf3b60f6f33cec857c5fb91e8582f567458b195
Dig Rat <CyberianDigRat@gmail.com>
2024-09-17 23:49:43 -0500

    script dedicated to launching the server with no gui.

--- Minecraft/start_server.sh
+++ Minecraft/start_server.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # https://minecraft.wiki/w/Tutorials/Setting_up_a_server#Java_options
+run_server()
+{
+    # Run the server file without gui.
+    java -jar server.jar --nogui
+    #java -Xmx1024M -Xms1024M -jar minecraft_server.1.21.1.jar nogui
+    #java -Xmx4G -Xms1G -XX:SoftMaxHeapSize=3G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar server.jar --nogui
+}
 
-java -jar server.jar --nogui
-
-java -Xmx1024M -Xms1024M -jar minecraft_server.1.21.1.jar nogui 
-
-java -Xmx4G -Xms1G -XX:SoftMaxHeapSize=3G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -jar server.jar --nogui
-
+# Main
+run_server
